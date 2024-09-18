1237ef3a017fecb9ae28aadb8ebcc50d6c638b01
kz23nyc <khinezar.mh@gmail.com>
2024-09-18 18:35:14 -0400

    third commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Content from './components/Content'
+import Footer from './components/Footer'
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <div>
+   <Header />
+   <Content />
+   <Content />
+   <Content />
+   <Footer />
+   </div>
+
+  )
+}
+
+export default App
---
+++ src/components/Content.jsx
@@ -0,0 +1,7 @@
+
+
+// import './Content.css'
+
+export default function Content() {
+    return (<p>This is my first React Application, wish me luck!</p>);
+  }
